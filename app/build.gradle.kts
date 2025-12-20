ebb65bbe8d60c28b969c4bbf467f68bdea234cc3
sleo441 <qrj.game@gmail.com>
2025-12-20 22:50:26 +0300

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,11 +9,11 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.homm3reference"
+        applicationId = "com.homm3.guidebook"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
